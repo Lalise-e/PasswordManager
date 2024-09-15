b32b024f8c1a537cb29b79487dcefbe25bd7088f
Alice <sten123465@gmail.com>
2024-09-16 01:27:32 +0200

    Revert "Massive brain"
    This reverts commit 2d1f9a494306c500166db19fe0c4e00585724751.

--- Testing Ground/Program.cs
+++ Testing Ground/Program.cs
@@ -1,28 +1,12 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Text.RegularExpressions;
-using Password;
 
 namespace rofl
 {
-   class Program
-   {
-      public static void Main()
-      {
-			EncryptedFile.FileDirectory = Directory.GetCurrentDirectory() + @"\Eeeeee";
-         byte[] key = new byte[32];
-         for (int i = 0; i < key.Length; i++)
-			{
-				key[i] = (byte)i;
-			}
-         TextEntry text = EncryptedFile.GetFile(key, @"C:\Users\alice\source\repos\PasswordManager\Testing Ground\bin\Debug\net6.0\Eeeeee\8937290756000345436.entry") as TextEntry;
-			//TextEntry text = new(key)
-			//{
-			//   Text = "Meri is super great and I big love him",
-			//   Title = "Feelings"
-			//};
-			//text.Save();
-		}
-   }
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("lmao nerd");
+        }
+    }
 }
