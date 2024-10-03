b3cc16d8ed5a0c024cac0c2a3a9e00e0630404f2
Alice <sten123465@gmail.com>
2024-10-04 00:20:25 +0200

    Renamed Settings Class

---
+++ Manager/Settings_old.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace Manager
+{
+    public class Settings_old
+    {
+        public Settings_old()
+        {
+            FontName = "Segoe UI";
+            BackgroundLocation = null;
+            Incognito = false;
+        }
+        public string FontName { get; set; }
+        public string BackgroundLocation { get; set; }
+        public bool Incognito { get; set; }
+        public static Settings_old GetSettings(string file)
+        {
+            string data = File.ReadAllText(file);
+            return JsonSerializer.Deserialize<Settings_old>(data);
+        }
+        public static void SaveSettings(string file, Settings_old settings)
+        {
+            string data = JsonSerializer.Serialize(settings);
+            File.WriteAllText(file, data);
+        }
+    }
+}
